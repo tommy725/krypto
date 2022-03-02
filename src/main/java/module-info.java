d6606fc500f2378ed4f0eeefb99be7a19edbed42
Tommy <afteryou725@gmail.com>
2022-03-02 22:31:08 +0100

    Create maven.yml
    * Create maven.yml
    
    * change version

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
-module pl.krypto.krypto {
+module pl.krypto {
     requires javafx.controls;
     requires javafx.fxml;
-
 
     opens pl.krypto.frontend to javafx.fxml;
     exports pl.krypto.frontend;
